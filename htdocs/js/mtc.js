6a6e22081fbdd61a731596971126b936a6ad4abc
David Findley <david.findley@ou.edu>
2014-08-27 22:26:36 -0500

    js function to get ISO week

--- htdocs/js/mtc.js
+++ htdocs/js/mtc.js
@@ -1,7 +1,8 @@
-Date.prototype.getWeek = function() {
-	var date = new Date(this.getTime());
-	date.setHours(0, 0, 0, 0);
-	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-	var week1 = new Date(date.getFullYear(), 0, 4);
-	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+function getWeekNumber(d) {
+	d = new Date(+d);
+	d.setHours(0,0,0);
+	d.setDate(d.getDate() + 4 - (d.getDay()||7));
+	var yearStart = new Date(d.getFullYear(),0,1);
+	var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
+	return [d.getFullYear(), weekNo];
 }
